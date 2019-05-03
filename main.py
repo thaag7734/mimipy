cf22a229676b13fb93a4e6f9f050b7e563443aed
thaag7734 <48517168+thaag7734@users.noreply.github.com>
2019-05-03 09:06:11 -0400

    Add spaces to re patterns

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ while not done:
 	if not re.search(r"^<[a-z0-9 =\"]+/>$", line):
 		lineError = True
 	if not lineError:
-		if re.search("^<note[a-z0-9 =\"]+/>$", line):
+		if re.search("^<note [a-z0-9 =\"]+/>$", line):
 			note = Note()
-			letterMatch = re.search("^.+letter=\"([a-z])\"", line)
-			modMatch = re.search("^.+mod=\"([sfn])\"", line)
-			octaveMatch = re.search("^.+octave=(\d)", line)
-			durationMatch = re.search("^.+duration=((?:\d)\.\d+|\d)", line)
+			letterMatch = re.search("^.+ letter=\"([a-z])\"", line)
+			modMatch = re.search("^.+ mod=\"([sfn])\"", line)
+			octaveMatch = re.search("^.+ octave=(\d)", line)
+			durationMatch = re.search("^.+ duration=((?:\d)\.\d+|\d)", line)
 			if letterMatch:
 				note.letter = letterMatch[1]
 			if modMatch:
@@ -28,12 +28,12 @@ while not done:
 			if durationMatch:
 				note.duration = durationMatch[1]
 			music.add(note)
-		elif re.search("^<chnote[a-z0-9 =\"]+/>$", line):
+		elif re.search("^<chnote [a-z0-9 =\"]+/>$", line):
 			chnote = ChNote()
-			letterMatch = re.search("^.+letter=\"([a-g])\"", line)
-			modMatch = re.search("^.+mod=\"([sfn])\"", line)
-			octaveMatch = re.search("^.+octave=(\d)", line)
-			durationMatch = re.search("^.+duration=((?:\d)\.\d+|\d)", line)
+			letterMatch = re.search("^.+ letter=\"([a-g])\"", line)
+			modMatch = re.search("^.+ mod=\"([sfn])\"", line)
+			octaveMatch = re.search("^.+ octave=(\d)", line)
+			durationMatch = re.search("^.+ duration=((?:\d)\.\d+|\d)", line)
 			if letterMatch:
 				chnote.letter = letterMatch[1]
 			if modMatch:
@@ -46,30 +46,30 @@ while not done:
 			if durationMatch:
 				chnote.duration = durationMatch[1]
 			music.add(chnote)
-		elif re.search("^<rest[a-z0-9 =\"]+/>$", line):
+		elif re.search("^<rest [a-z0-9 =\"]+/>$", line):
 			rest = Rest()
-			letterMatch = re.search("^.+letter=\"([a-g])\"", line)
-			durationMatch = re.search("^.+duration=((?:\d)\.\d|\d)", line)
+			letterMatch = re.search("^.+ letter=\"([a-g])\"", line)
+			durationMatch = re.search("^.+ duration=((?:\d)\.\d|\d)", line)
 			if letterMatch:
 				rest.letter = letterMatch[1]
 			if durationMatch:
 				rest.duration = durationMatch[1]
 			music.add(rest)
-		elif re.search("^<chrest[a-z0-9 =\"]+/>$", line):
+		elif re.search("^<chrest [a-z0-9 =\"]+/>$", line):
 			chrest = ChRest()
-			letterMatch = re.search("^.+letter=\"([a-g])\"", line)
-			durationMatch = re.search("^.+duration=((?:\d)\.\d|\d)", line)
+			letterMatch = re.search("^.+ letter=\"([a-g])\"", line)
+			durationMatch = re.search("^.+ duration=((?:\d)\.\d|\d)", line)
 			if letterMatch:
 				chrest.letter = letterMatch[1]
 			if durationMatch:
 				chrest.duration = durationMatch[1]
 			music.add(chrest)
-		elif re.search("^<prop[a-z0-9 =\"]+/>$", line):
+		elif re.search("^<prop [a-z0-9 =\"]+/>$", line):
 			prop = Properties()
-			tempoMatch = re.search("^.+tempo=(\d)", line)
-			keyMatch = re.search("^.+key=\"([a-g](?:[sfn])?)\"", line)
-			tstopMatch = re.search("^.+tstop=(\d)", line)
-			tsbtmMatch = re.search("^.+tsbtm=(\d)", line)
+			tempoMatch = re.search("^.+ tempo=(\d)", line)
+			keyMatch = re.search("^.+ key=\"([a-g](?:[sfn])?)\"", line)
+			tstopMatch = re.search("^.+ tstop=(\d)", line)
+			tsbtmMatch = re.search("^.+ tsbtm=(\d)", line)
 			if tempoMatch:
 				prop.tempo = tempoMatch[1]
 			if keyMatch:
