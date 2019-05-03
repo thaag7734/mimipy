61c70b0ce107365a543c2743db887bf4437581eb
thaag7734 <48517168+thaag7734@users.noreply.github.com>
2019-05-03 09:00:11 -0400

    Move all classes to objects.py

--- objects.py
+++ objects.py
@@ -1,3 +1,5 @@
+
+
 class Note:
 	def __init__(self, letter="c", mod="", octave=5, duration=1):
 		self.letter = letter
@@ -32,3 +34,27 @@ class Properties:
 		self.key = key
 		self.timesig = (int(tstop),int(tsbtm))
 		self.a = int(a)
+
+class Music:
+	def __init__(self, prop=Properties()):
+		self.musicList = []
+		self.add(prop)
+
+	def add(self, item):
+		self.musicList.append(item)
+		
+	def display(self):
+		print(self.musicList)
+		for item in self.musicList:
+			if type(item) is Note:
+				print("Note: %s%s%s;%s" % (item.letter, item.mod, str(item.octave), str(item.duration)))
+			elif type(item) is ChNote:
+				print("ChNote: %s%s%s;%s" % (item.letter, item.mod, str(item.octave), str(item.duration)))
+			elif type(item) is Rest:
+				print("Rest: %s;%s" % (item.letter, str(item.duration)))
+			elif type(item) is ChRest:
+				print("Rest: %s;%s" % (item.letter, str(item.duration)))
+			elif type(item) is Properties:
+				print("Prop: %sbpm;%s;%s/%s" % (str(item.tempo), item.key, str(item.timesig[0]), str(item.timesig[1])))
+			elif type(item) is ChStep:
+				print("ChStep")
