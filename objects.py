681900da330e0ef144ed83071a7d486fc878813e
thaag7734 <haag-56632@cotcmail.cotc.edu>
2019-05-14 08:24:55 -0400

    Continue syntax migration

--- objects.py
+++ objects.py
@@ -19,36 +19,22 @@ def sine_tone(f, duration, stream):
 
 
 class Note:
-	def __init__(self, letter="c", mod="", octave=5, duration=1):
+	def __init__(self, letter="c", octave=5, duration=1):
 		self.letter = letter
-		self.mod = mod
 		self.octave = int(octave)
 		self.duration = float(duration)
 	
 	def play(self, a, tempo, key, p):
-		keyMod = self.mod
-		if keyMod == "":
-			keyMod = KEYS[key][self.letter]
-		n = SEMITONES[self.letter + keyMod]
+		sine_tone(hz, 60/tempo*self.duration, p)
+
+	def setFrequency():
+		if not self.letter[1]:
+			letter = KEYS[key][self.letter]
+		else:
+			letter = self.letter
+		n = SEMITONES[letter]
 		octaveDiff = self.octave - 4
 		hz = a*2**(((n+octaveDiff*12)-9)/12)
-		sine_tone(hz, 60/tempo*self.duration, p)
-		
-class ChNote:
-	def __init__(self, letter="c", mod="", octave=5, duration=1):
-		self.letter = letter
-		self.mod = mod
-		self.octave = int(octave)
-		self.duration = float(duration)
-	
-	def play(self, a, tempo, key, p):
-		keyMod = self.mod
-		if keyMod == "":
-			keyMod = KEYS[key][self.letter]
-		n = SEMITONES[self.letter + keyMod]
-		octaveDiff = self.octave - 4
-		hz = a*2**(((n+octaveDiff*12)-9)/12)
-		sine_tone(hz, 60/tempo*self.duration, p)
 
 class Rest:
 	def __init__(self, letter="b", duration=1):
@@ -57,54 +43,18 @@ class Rest:
 	
 	def rest(self, tempo):
 		time.sleep(60/tempo*self.duration)
-		
-class ChRest:
-	def __init__(self, letter="f", duration=1):
-		self.letter = letter
-		self.duration = float(duration)
-	
-	def rest(self, tempo, e):
-		restThread = threading.Thread(target=self.chrest, args=(tempo, e))
-		
-	def chrest(self, tempo, e):
-		e.set()
-		time.sleep(60/tempo*self.duration)
-		e.clear()
-		
-class ChStep:
-	def __init__(self):
-		pass
 
-class Properties:
-	def __init__(self, tempo=80, key="c", tstop=4, tsbtm=4, a=440):
-		self.tempo = int(tempo)
-		self.key = key
-		self.timesig = (int(tstop),int(tsbtm))
-		self.a = float(a)
+class Meta:
+	def __init__(self, meta):
+		self.meta = meta
+		self.title = None
+		self.tempo = None
 
-class Music:
-	def __init__(self, prop=Properties()):
-		self.musicList = []
-		self.add(prop)
+class MIMIError(Exception):
+	pass
 
-	def add(self, item):
-		self.musicList.append(item)
-		
-	def display(self):
-		print(self.musicList)
-		for item in self.musicList:
-			if type(item) is Note:
-				print("Note: %s%s%s;%s" % (item.letter, item.mod, str(item.octave), str(item.duration)))
-			elif type(item) is ChNote:
-				print("ChNote: %s%s%s;%s" % (item.letter, item.mod, str(item.octave), str(item.duration)))
-			elif type(item) is Rest:
-				print("Rest: %s;%s" % (item.letter, str(item.duration)))
-			elif type(item) is ChRest:
-				print("Rest: %s;%s" % (item.letter, str(item.duration)))
-			elif type(item) is Properties:
-				print("Prop: %sbpm;%s;%s/%s" % (str(item.tempo), item.key, str(item.timesig[0]), str(item.timesig[1])))
-			elif type(item) is ChStep:
-				print("ChStep")
+class InvalidValueError(MIMIError):
+	pass
 
-class LineError(Exception):
+class MissingElementError(MIMIError):
 	pass
--- playfile.py
+++ playfile.py
@@ -1,9 +1,11 @@
 import re
-from objects import Note, ChNote, Rest, ChRest, Properties, ChStep, LineError
+from objects import *
 import pyaudio
 from pyaudio import PyAudio
 import os
 import threading
+import defusedxml.ElementTree as ET
+from defusedxml.ElementTree import ParseError
 
 p = PyAudio()
 
@@ -11,12 +13,24 @@ chrestEvent = threading.Event()
 
 FILTERS = {
 	"float": re.compile(r"^(\d+|(?:\d+)?\.\d+)$"),
-	"int": re.compile(r"^(\d+)$")
+	"int": re.compile(r"^(\d+)$"),
+	"letter": re.compile(r"^([A-G](?:[#b])?)$")
 	}
 
 
 				
-def playMusic(music, p):
+def playMusic(music, meta):
+	for beat in music.iter("beat"):
+		for note in beat.iter("note"):
+			n = Note()
+			n.letter = note.get("letter")
+			if not FILTERS["letter"].match(n.letter):
+				raise InvalidValueError("Note letter must be from A-G inclusive, with optional # or b modifier.")
+			n.duration = note.find("duration").text
+			try:
+				n.octave = note.find("octave").text
+			except ParseError:
+				n.octave = 4
 
 def mainMenu(p):
 	stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=True)
@@ -24,7 +38,22 @@ def mainMenu(p):
 	while not done:
 		filename = input("Enter path to file:\n")
 		if os.path.isfile(filename):
-			#do xml init stuff here
+			tree = ET.parse(filename)
+			root = tree.getroot()
+
+			meta = Meta(root.find("meta"))
+			meta.title = meta.meta.find("title").text
+			if not meta.title:
+				raise MissingElementError("Title tag is missing from song metadata.")
+			meta.tempo = meta.meta.find("tempo").text
+			if not FILTERS["int"].match(meta.tempo):
+				raise InvalidValueError("Tempo must be an integer.")
+
+			music = root.find("music")
+
+			print("Now playing: '%s'" % meta.title)
+			playMusic(music, meta)
+
 			done = True
 		else:
 			print("File '%s' does not exist." % filename)
